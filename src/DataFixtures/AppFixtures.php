dddbf3b4dadaaa71687e25676a6955eddb1a15b7
Jean-Pierre <jean-pierre.jounier@ac-bordeaux.fr>
2022-09-29 10:28:22 +0200

    Entité User, maj fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,35 +4,56 @@ namespace App\DataFixtures;
 
 use App\Entity\Customer;
 use App\Entity\Invoice;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+
+    public function __construct(private UserPasswordHasherInterface $encoder)
+    {
+    }
+
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create('fr_FR');
-        $chrono = 1;
-        for($c = 0 ; $c < 30; $c++) {
-            $customer = new Customer();
-            $customer->setFirstName($faker->firstName)
-                ->setLastName($faker->lastName)
-                ->setCompany($faker->company)
-                ->setEmail($faker->email);
-            $manager->persist($customer);
 
-            for($i = 0 ; $i < mt_rand(3,10); $i++) {
-                $invoice = new Invoice();
-                $invoice->setAmount($faker->randomFloat(2, 250, 5000))
-                    ->setSentAt($faker->dateTimeBetween('-6 months'))
-                    ->setStatus($faker->randomElement(['SENT', 'PAID', 'CANCEL']))
-                    ->setCustomer($customer)
-                    ->setChrono($chrono);
-                $manager->persist($invoice);
-                $chrono++;
+        for ($u = 0; $u < 10; $u++) {
+            $chrono = 1;
+            $user = new User();
+            $hash = $this->encoder->hashPassword($user, 'password');
+            $user->setFirstName($faker->firstName)
+                ->setLastName($faker->lastName)
+                ->setEmail($faker->email)
+                ->setPassword($hash);
+            $manager->persist($user);
+
+            for($c = 0 ; $c < mt_rand(5,20); $c++) {
+                $customer = new Customer();
+                $customer->setFirstName($faker->firstName)
+                    ->setLastName($faker->lastName)
+                    ->setCompany($faker->company)
+                    ->setEmail($faker->email)
+                    ->setUser($user);
+                $manager->persist($customer);
+
+                for($i = 0 ; $i < mt_rand(3,10); $i++) {
+                    $invoice = new Invoice();
+                    $invoice->setAmount($faker->randomFloat(2, 250, 5000))
+                        ->setSentAt($faker->dateTimeBetween('-6 months'))
+                        ->setStatus($faker->randomElement(['SENT', 'PAID', 'CANCEL']))
+                        ->setCustomer($customer)
+                        ->setChrono($chrono);
+                    $manager->persist($invoice);
+                    $chrono++;
+                }
             }
         }
+
+
 
         $manager->flush();
     }
--- src/Entity/Customer.php
+++ src/Entity/Customer.php
@@ -30,6 +30,9 @@ class Customer
     #[ORM\OneToMany(mappedBy: 'customer', targetEntity: Invoice::class)]
     private Collection $invoices;
 
+    #[ORM\ManyToOne(inversedBy: 'customers')]
+    private ?User $user = null;
+
     public function __construct()
     {
         $this->invoices = new ArrayCollection();
@@ -114,6 +117,18 @@ class Customer
                 $invoice->setCustomer(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
 
         return $this;
     }
