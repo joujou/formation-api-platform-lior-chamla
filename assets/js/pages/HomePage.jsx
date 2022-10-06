774b8f31809d39df2a50e3459f4c2fc03c6cd89a
Jean-Pierre <jean-pierre.jounier@ac-bordeaux.fr>
2022-10-06 10:51:30 +0200

    début react

---
+++ assets/js/pages/HomePage.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div className="alert alert-primary">
+      <h4 className="alert-heading">Bienvenue</h4>
+      <p className="mb-0">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus
+        consequatur ea eum eveniet harum quam recusandae rem? Aperiam commodi
+        consectetur cumque deleniti deserunt dignissimos dolorem ea eaque eos
+        esse est et excepturi explicabo fuga illum impedit, itaque labore
+        laudantium libero magnam maxime nisi officia pariatur possimus quia
+        quibusdam quisquam repellendus saepe sequi soluta tempora ullam vel
+        veritatis. Ab atque debitis ea esse ex omnis quia recusandae rerum
+        totam. Consequatur dolor dolore eius eligendi facilis harum, id iusto
+        laboriosam maxime pariatur provident quidem quos ratione recusandae rem
+        soluta sunt voluptate. Consequatur, debitis iusto. Adipisci amet
+        consequuntur distinctio expedita incidunt natus necessitatibus nemo
+        nulla provident, quibusdam reiciendis repellendus, unde vero. Dicta esse
+        iure rem tempora vel.
+      </p>
+    </div>
+  )
+}
+export default HomePage
--- src/Doctrine/CurrentUserExtension.php
+++ src/Doctrine/CurrentUserExtension.php
@@ -20,13 +20,14 @@ class CurrentUserExtension implements QueryCollectionExtensionInterface, QueryIt
 
     }
 
-    private function addWhere(QueryBuilder $queryBuilder, string $resourceClass) {
+    private function addWhere(QueryBuilder $queryBuilder, string $resourceClass)
+    {
         $user = $this->security->getUser();
-        if(($resourceClass === Customer::class || $resourceClass === Invoice::class) && !$this->authorizationChecker->isGranted('ROLE_ADMIN') && $user instanceof User) {
+        if (($resourceClass === Customer::class || $resourceClass === Invoice::class) && !$this->authorizationChecker->isGranted('ROLE_ADMIN') && $user instanceof User) {
             $rootAlias = $queryBuilder->getRootAliases()[0];
-            if($resourceClass === Customer::class) {
+            if ($resourceClass === Customer::class) {
                 $queryBuilder->andWhere("$rootAlias.user = :user");
-            } elseif($resourceClass === Invoice::class) {
+            } elseif ($resourceClass === Invoice::class) {
                 $queryBuilder->join("$rootAlias.customer", "c")
                     ->andWhere("c.user = :user");
             }
