e0b5d4156cd60f89d83a2aec8736b24ab13dfdbc
jpj <jpjounier@gmail.com>
2022-10-06 16:20:04 +0200

    pagination supression

--- assets/js/pages/CustomersPage.jsx
+++ assets/js/pages/CustomersPage.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import Pagination from '../components/Pagination'
 const CustomersPage = () => {
   const [customers, setCustomers] = useState([])
+  const [currentPage, setCurrentPage] = useState(1)
   useEffect(() => {
     axios
       .get('http://localhost:8000/api/customers')
@@ -9,6 +11,34 @@ const CustomersPage = () => {
       .then((data) => setCustomers(data))
       .catch((error) => console.log(error.response))
   }, [])
+
+  const handleDelete = (id) => {
+    // Copie des customers pour faire un mix optimiste/pessimiste
+    const originalCustomers = [...customers]
+
+    // Optimiste
+    setCustomers(customers.filter((customer) => customer.id !== id))
+
+    // Pessimiste = supprimer uniquement quand reponse requete delete
+
+    axios
+      .delete(`http://localhost:8000/api/customers/${id}`)
+      .then((response) => console.log(response))
+      .catch((error) => {
+        setCustomers(originalCustomers)
+        console.log(error.response)
+      })
+  }
+
+  const itemsPerPage = 10
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page)
+  }
+
+  const start = currentPage * itemsPerPage - itemsPerPage
+  const paginatedCustomers = customers.slice(start, start + itemsPerPage)
+
   return (
     <>
       <h1>Clients</h1>
@@ -24,7 +54,7 @@ const CustomersPage = () => {
           </tr>
         </thead>
         <tbody>
-          {customers.map((customer) => (
+          {paginatedCustomers.map((customer) => (
             <tr key={customer.id}>
               <td>{customer.id}</td>
               <td>
@@ -34,10 +64,26 @@ const CustomersPage = () => {
               <td>{customer.company}</td>
               <td>{customer.invoices.length}</td>
               <td>{customer.totalAmount.toLocaleString()} €</td>
+              <td>
+                <button
+                  onClick={() => handleDelete(customer.id)}
+                  disabled={customer.invoices.length > 0}
+                  className="btn btn-sm btn-danger"
+                >
+                  Supprimer
+                </button>{' '}
+              </td>
             </tr>
           ))}
         </tbody>
       </table>
+
+      <Pagination
+        currentPage={currentPage}
+        itemsPerPage={itemsPerPage}
+        length={customers.length}
+        onPageChanged={handlePageChange}
+      />
     </>
   )
 }
